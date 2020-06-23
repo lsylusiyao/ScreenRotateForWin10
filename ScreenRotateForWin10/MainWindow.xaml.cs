3d8519dfa6ed5347ead0019820cbe34646dd62aa
lsylusiyao <lsylusiyao@qq.com>
2020-06-23 17:30:15 +0800

    add showing current screen num and primary screen num

--- ScreenRotateForWin10/MainWindow.xaml.cs
+++ ScreenRotateForWin10/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Interop;
 
 namespace ScreenRotateForWin10
 {
@@ -19,19 +21,29 @@ namespace ScreenRotateForWin10
 
         private List<string> screenList = new List<string>();
         private int screenCount = 0;
+        private int currScreenID = 0;
+        private int primaryScreenID = 0;
+        private readonly List<Screen> allScreens = Screen.AllScreens.ToList();
 
         private void Init()
         {
             var result = new List<string>();
-            screenCount = Screen.AllScreens.Count();
+            
+            screenCount = allScreens.Count;
             result.AddRange(
                 Enumerable.Range(0, screenCount)
-                .Select(x => $"{x + 1}号屏幕") // $"NO.{x} Screen"
+                .Select(x => $"{x + 1}号屏幕") // $"NO.{x + 1} Screen"
                 );
             result.Add("所有"); // All
+
+            primaryScreenID = allScreens.IndexOf(Screen.PrimaryScreen) + 1;
+            primaryScreenLabel.Content = $"主屏幕：{primaryScreenID}号屏幕"; // "Primary Screen: NO.? Screen"
+
             screenList = result;
             choiceComboBox.ItemsSource = screenList;
             choiceComboBox.SelectedIndex = result.Count - 1; // Last
+
+            Window_LocationChanged(null, null); // currScreen label changed
         }
 
         private void RotateChoice(int choice, Display.Orientations degree)
@@ -72,6 +84,13 @@ namespace ScreenRotateForWin10
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
             RotateChoice(choiceComboBox.SelectedIndex, Display.Orientations.DEGREES_CW_0);
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            var currScreen = Screen.FromHandle(new WindowInteropHelper(this).Handle);
+            currScreenID = allScreens.IndexOf(currScreen) + 1;
+            currScreenLabel.Content = $"当前：{currScreenID}号屏幕"; // "Curr: NO.? Screen"
         }
     }
 }
